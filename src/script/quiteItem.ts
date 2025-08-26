9a1556bb978981e9134f48bf9cb471826d21fae1
xingwangzhe <xingwangzhe@outlook.com>
2025-08-26 09:10:21 +0800

    fs依赖更新

--- src/script/quiteItem.ts
+++ src/script/quiteItem.ts
@@ -1,10 +1,23 @@
 import { MenuItem } from "@tauri-apps/api/menu/menuItem";
-import { exit } from '@tauri-apps/plugin-process';
-
+import { exit } from "@tauri-apps/plugin-process";
+import {
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from "@tauri-apps/plugin-notification";
+import { invoke } from '@tauri-apps/api/core';
+await invoke('close_app');
 export const quitItem: MenuItem = await MenuItem.new({
   id: "quitItem",
   text: "退出应用",
   action: async () => {
-    exit(0);
+    let permissionGranted = await isPermissionGranted();
+    if (!permissionGranted) {
+      const permission = await requestPermission();
+      permissionGranted = permission === "granted";
+    }
+    sendNotification({ title: "Wngtools", body: "wngtools已关闭" });
+    await exit(0);
+    // await invoke('close_app');
   },
 });
